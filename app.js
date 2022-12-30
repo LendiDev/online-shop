b38c0eba4d1fd47cd2f7b2ae252bfd521f3109c5
Nikita Lendiak <lendiak93@gmail.com>
2022-12-30 22:21:36 +0000

    Add view template handler and couple basic error pages

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
-
+const path = require('path');
 const express = require("express");
 
 const app = express();
 
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-  res.send("all good");
+  res.render('home');
 });
+
+app.use((error, req, res, next) => {
+  res.status(500).render('500');
+})
+
+app.use('*', (req, res) => {
+  res.status(404).render('404');
+})
 
 app.listen(process.env.PORT || 3000);
