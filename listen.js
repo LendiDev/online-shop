42d2fa428d5d66bc29738e2fffaa67cc42c395e1
Nikita Lendiak <lendiak93@gmail.com>
2023-04-02 20:12:43 +0100

    update for cyclic

--- listen.js
+++ listen.js
@@ -1,16 +1,38 @@
 const db = require("./database/database");
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 db.connectToDatabase()
-  .then(() => {
-    if (process.env.NODE_ENV !== "test") {
-      app.listen(PORT);
-      console.log(`Server listening on port: ${PORT}`);
-    }
+  .then((client) => {
+    client.connect((err) => {
+      if (err) {
+        console.error(err);
+        return false;
+      }
+      // connection to mongo is successful, listen for requests
+      app.listen(PORT, () => {
+        console.log("listening for requests");
+      });
+    });
+
+    // if (process.env.NODE_ENV !== "test") {
+    //   app.listen(PORT);
+    //   console.log(`Server listening on port: ${PORT}`);
+    // }
   })
   .catch((error) => {
     console.log("Failed to connect to database");
     console.log(error);
   });
+
+// db.connectToDatabase().connect((err) => {
+//   if (err) {
+//     console.error(err);
+//     return false;
+//   }
+
+//   app.listen(PORT, () => {
+//     console.log("listening for requests");
+//   });
+// });
