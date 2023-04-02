f1d5cda22b2659bd7d18eb3d89ce2c63b5323270
Nikita Lendiak <lendiak93@gmail.com>
2023-04-02 19:44:00 +0100

    update image storage

--- middlewares/uploads.middleware.js
+++ middlewares/uploads.middleware.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 
 const imageUploadStorageConfig = multer.diskStorage({
-  destination: "uploads/product-images",
+  destination: "tmp/product-images",
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${uuidv4()}-${file.originalname}`);
   },
